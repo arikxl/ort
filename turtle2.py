e343d54fcdd23bd55e2a021d874b5be08be0b4f9
Arik Alexandrov <arikxl@gmail.com>
2024-02-01 00:08:16 +0200

    1

---
+++ turtle2.py
@@ -0,0 +1,22 @@
+import turtle
+# Create a screen
+screen = turtle.Screen()
+screen.bgcolor('hotPink')
+
+# Create a turtle
+player = turtle.Turtle()
+player.shape('turtle')
+
+
+
+
+# player.left(90)
+player.fd(150)
+player.left(180)
+player.fd(75)
+player.right(90)
+player.fd(150)
+# player.left(125)
+# player.fd(30)
+
+turtle.done()
