c5a9fd8d178d4c62d6f8109ac09696a1a5ecccff
Arik Alexandrov <arikxl@gmail.com>
2024-02-28 23:31:51 +0200

    some loops

---
+++ some-loops.py
@@ -0,0 +1,67 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor('hotPink')
+
+player = turtle.Turtle()
+player.shape('turtle')
+
+
+# stairs
+# for i in range(3):
+#     player.forward(30)
+#     player.left(90)
+#     player.forward(30)
+#     player.right(90)
+
+# squre
+# for i in range(4):
+#     player.forward(30)
+#     player.left(90)
+
+# tringle
+# for i in range(3):
+#     player.forward(100)
+#     player.left(120)
+
+# for _ in range(5):  # Draw 5 lines to form a star
+#     player.forward(100)
+#     player.right(144)
+
+
+# for _ in range(6):  # Draw a hexagon
+#     player.forward(100)
+#     player.right(60)
+
+
+ # Rotate the turtle slightly
+
+# circle
+# player.circle(100)
+
+# radii = [100, 75, 50]
+
+# # Loop through each radius and draw the circles
+# for radius in radii:
+#     player.circle(radius)
+
+
+# for _ in range(36):  # Draw 36 squares
+#     for _ in range(4):  # Draw a square
+#         player.forward(100)
+#         player.right(90)
+#     player.right(10)
+
+# start
+
+
+
+# length = 10
+# for _ in range(60):  # Draw 60 triangles
+#     for _ in range(3):  # Draw a triangle
+#         player.forward(length)
+#         player.left(120)
+#     player.forward(length)
+#     player.left(5) 
+
+turtle.done()
